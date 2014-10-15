1fc30b1db3e4fff599601afea62d97b987932fd6
Brandon Johnson <brandon@apptys.com>
2014-10-14 20:56:06 -0600

    Initial Commit

---
+++ OutProform/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OutProform
+//
+//  Created by Brandon Johnson on 10/14/14.
+//  Copyright (c) 2014 OutProform. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OutProform/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OutProform
+//
+//  Created by Brandon Johnson on 10/14/14.
+//  Copyright (c) 2014 OutProform. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
